4742c0650cd433a0b93b38c82cd0b318c6309f00
AyaDesigner <aya.berdyeva@gmail.com>
2020-11-18 14:47:12 +0100

    added random dogs api

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -11,16 +11,20 @@ class Quotes extends React.Component {
         this.state = { 
             
             quote: "Loading..." ,
-            img: "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
-    
-    
+            img: "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif",
+            imgSizeBites: 2748669
     };
         
     }
 
     //life cycle
     componentDidMount() {
+        this.getQuotesAndImages();
+    }
+
+    getQuotesAndImages(){
         this.getQuotes();
+        this.getImage();
     }
 
     getQuotes() {
@@ -31,7 +35,21 @@ class Quotes extends React.Component {
             })
             .then((jsonObject) => {
                 console.log("this what is returning: " + jsonObject.value);
-                this.setState({ quote: jsonObject.value, img: "https://media1.tenor.com/images/00ae7fd2b2acbdc7b36600c73941ec6a/tenor.gif?itemid=9540227"})
+                this.setState({ quote: jsonObject.value})
+            })
+            .catch(console.log)
+
+    }
+
+    getImage() {
+
+        fetch('https://random.dog/woof.json')
+            .then(objectResponse => {
+                return objectResponse.json();
+            })
+            .then((jsonObject) => {
+                console.log("this what is returning img: " + jsonObject.url);
+                this.setState({ img: jsonObject.url, imgSizeBites: jsonObject.fileSizeBytes})
             })
             .catch(console.log)
 
@@ -43,7 +61,7 @@ class Quotes extends React.Component {
             <div>
                 <img src = {this.state.img} alt = "Chuck"></img>
                 <h2>{this.state.quote}</h2>
-                <Button variant="success" onClick={() => this.getQuotes()}>Chuck would click here</Button>
+                <Button variant="success" onClick={() => this.getQuotesAndImages()}   >Chuck would click here</Button>
             </div>
         )
     }
